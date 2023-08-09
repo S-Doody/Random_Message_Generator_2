cb3e1995aa9111bc87f43b4c1e449de62cb1b825
S-Doody <seanjd90@gmail.com>
2023-08-09 15:32:25 -0400

    Add array for first part of joke

---
+++ main.js
@@ -0,0 +1,8 @@
+/*
+This is a random message generator containing three separate data types. 
+This program will produce random nonsensical jokes. 
+*/
+
+//Array of first part of joke
+const jokePartOne = ['Knock knock. Who\'s there? ', 'Why did the ', 'Did you hear about ', 'What did one ocean say to ', 'A priest, a rabbi, and a minister walk into a bar... ']
+
