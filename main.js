520615e2e54315414041438bba61e0aacfcccf50
S-Doody <seanjd90@gmail.com>
2023-08-09 15:41:16 -0400

    Add array of second part of joke

--- main.js
+++ main.js
@@ -4,5 +4,8 @@ This program will produce random nonsensical jokes.
 */
 
 //Array of first part of joke
-const jokePartOne = ['Knock knock. Who\'s there? ', 'Why did the ', 'Did you hear about ', 'What did one ocean say to ', 'A priest, a rabbi, and a minister walk into a bar... ']
+const jokePartOne = ['Knock knock. Who\'s there? ', 'Why did the ', 'Why don\'t scientists', 'What did one ocean say to ', 'A priest, a rabbi, and a minister walk into a bar... ']
+
+//Array of second part of joke
+const jokePartTwo = ['Lettuce. Lettuce who? ', 'chicken cross the road? ', 'trust atoms? ', 'the other ocean? ', 'The bartender looks up and says ']
 
